051d8f16aba25852a99d4ed1a5c1619a34c2a25b
Yaroslav-siberia <56403906+Yaroslav-siberia@users.noreply.github.com>
2022-01-27 20:28:09 +0200

    upgrading

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from recognition import recognition
+from kaldi_recognition.recognition import recognition
 import sys
-from semantic import check_answer_type
+from utils.semantic import check_answer_type
+
 
 audio = sys.argv[1]
 answer_type = check_answer_type(recognition(audio))
