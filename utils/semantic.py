3c1b398c0afd609e3104c8fa3cb23571559b2ec9
Yaroslav-siberia <56403906+Yaroslav-siberia@users.noreply.github.com>
2022-01-27 20:01:29 +0200

    add lemmatizing and semantic distance

---
+++ utils/semantic.py
@@ -0,0 +1,39 @@
+import spacy
+import json
+nlp = spacy.load('ru_core_news_lg')
+with open('answers.json') as f:
+    answer_types = json.load(f)
+
+'''
+по полученному ответу (строке) определяет какая ветка разговора выбрана
+'''
+def lemmatization(sentence1: spacy.tokens.doc.Doc, sentence2: spacy.tokens.doc.Doc):
+    sentence1 = nlp(" ".join([word.lemma_ for word in sentence1]))
+    sentence2 = nlp(" ".join([word.lemma_ for word in sentence2]))
+    return sentence1, sentence2
+
+lemmatization('От топота копыт пыль по полю летит. Собака злится и рычит')
+
+def check_answer_type(answer: str, lemmatizing = False )->str:
+    answer = nlp(answer)
+    #type(answer) = spacy.tokens.doc.Doc
+    distance = 0
+    right_key = ""
+    for key in answer_types:
+        for item in answer_types[key]:
+            example = nlp(item)
+            if answer.similarity(example) > distance:
+                distance = answer.similarity(example)
+                right_key = key
+                if distance == 1.0:
+                    return right_key
+            # если включена лемматизация то повторяем процедуру поиска расстояния но с лемматизированными выраженриями
+            if lemmatizing == True:
+                answer_lem, example_lem = lemmatizing(answer,example)
+                if answer_lem.similarity(example_lem) > distance:
+                    distance = answer_lem.similarity(answer_lem)
+                    right_key = key
+                    if distance == 1.0:
+                        return right_key
+    return right_key
+
